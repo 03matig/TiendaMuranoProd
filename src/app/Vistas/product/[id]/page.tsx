627af0d26ac0812a281873dffea40c94cf346707
03matig <matias.garin@aiwolf.dev>
2025-06-05 11:38:44 -0400

    Eliminación de enrutamiento dinámico.

--- src/app/Vistas/product/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import ProductDetail from "@/app/UI/ProductDetail";
-
-export default async function ProductPage({ params }: { params: { id: string } }) {
-  if (!params || !params.id) {
-    return <p>Cargando...</p>; // Evita acceder a `params.id` si no está disponible
-  }
-
-  return <ProductDetail productId={params.id} />;
-}
---
+++ src/app/Vistas/product/page.tsx
@@ -0,0 +1,7 @@
+// src/app/Vistas/product/page.tsx
+
+import ProductDetail from "@/app/UI/ProductDetail";
+
+export default function Page() {
+  return <ProductDetail />;
+}
